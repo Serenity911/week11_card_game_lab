db6a312201e75061ba688d3ff08527267aca1adf
Serenity911 <silvia.simonassi01@gmail.com>
2020-02-16 12:07:40 +0000

    player can stand

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,10 +5,12 @@ public class Player extends Person {
 
     private String name;
     private ArrayList<Card> aces;
+    private boolean stand;
 
     public Player(String name){
         this.name = name;
         this.aces = new ArrayList<Card>();
+        this.stand = false;
 
     }
 
@@ -33,18 +35,40 @@ public class Player extends Person {
     }
 
     public String getNextAction(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Stand or Twist?");
-        String playerInput = scanner.nextLine();
-        scanner.close();
-        return playerInput;
+        if (!isBusted && !stand) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Stand or Twist?");
+            String playerInput = scanner.nextLine();
+            scanner.close();
+            return playerInput;
+        }
+        else {
+            return "no more round";
+        }
     }
 
-//    public String getNextAction() {
-//
-//    }
+    public void playAction(String playerInput) {
+        if(playerInput == "no more round") {
+            return;
+        }
+        if(playerInput == "Stand"){
+            this.chooseStand();
+        }
+        if(playerInput == "Twist") {
+            this.chooseTwist();
+        }
+        else {
+            System.out.println("Invalid, try again.");
+            this.getNextAction();
+        }
+    }
+
+    public boolean getDoesStand() {
+        return this.stand;
+    }
 
     public boolean chooseStand(){
+        this.stand = true;
         return true;
     }
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -25,6 +25,8 @@ public class PlayerTest {
         player1.addToHand(cardTwo);
         player1.chooseStand();
         assertEquals(2, player1.countHandCards());
+        assertEquals(true, player1.getDoesStand());
+
     }
 
     @Test
@@ -36,6 +38,15 @@ public class PlayerTest {
     }
 
     @Test
+    public void cannot_twist_if_stand(){
+        player1.addToHand(cardAce);
+        player1.addToHand(cardTwo);
+        player1.chooseStand();
+        player1.chooseTwist();
+        assertEquals(2, player1.countHandCards());
+    }
+
+    @Test
     public void asks_input(){
         String input = "test";
         InputStream in = new ByteArrayInputStream(input.getBytes());
@@ -43,4 +54,18 @@ public class PlayerTest {
         String personInput = player1.getNextAction();
         assertEquals("test", personInput );
     }
+
+    @Test
+    public void do_not_ask_input_if_stand(){
+//        String input = "test";
+//        InputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+        player1.addToHand(cardAce);
+        player1.addToHand(cardTwo);
+        player1.chooseStand();
+        String personInput = player1.getNextAction();
+        assertEquals("no more round", personInput );
+    }
+
 }
+
