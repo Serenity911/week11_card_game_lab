d15da80d7bcc1c8f32ffe216d4e2b44d3ec8d8cc
Serenity911 <silvia.simonassi01@gmail.com>
2020-02-17 09:32:26 +0000

    removed comment

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -33,17 +33,6 @@ public class Runner {
 
         System.out.println("The winner is...:");
         System.out.println(game.findWinner().getName());
-//        System.out.println(game.getPlayers()[0].getHandValue());
 
-
-//        for (Person player : game.getPlayers()) {
-//            System.out.println(""+ player.getHand().getRank() + " of " + player.getHand().getSuit());
-//        }
-//        Person winner = game.findWinner();
-//        System.out.println("the winner is: " + winner.getName());
-//        System.out.println(""+ winner.getName() + " has " + winner.getScore() + " points" );
-//        winner = game.newRound();
-//        System.out.println("the winner is: " + winner.getName());
-//        System.out.println(""+ winner.getName() + " has " + winner.getScore() + " points" );
     }
 }
